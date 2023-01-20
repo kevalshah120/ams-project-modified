9e95901ad05924864bf7c57a5e5cfa6fe91e2e12
Yujii-Itadori <xofficialyashx@gmail.com>
2023-01-20 20:44:47 +0530

    Student_login done
    Ho gaya bhai pawri karo

--- app/src/main/java/com/example/splashscreen/student_login.java
+++ app/src/main/java/com/example/splashscreen/student_login.java
@@ -2,16 +2,29 @@ package com.example.splashscreen;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import java.util.HashMap;
+import java.util.Map;
+
 public class student_login extends AppCompatActivity {
     Button back,login;
     EditText mobile_no,enr_no;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,25 +40,76 @@ public class student_login extends AppCompatActivity {
             finish();
         });
         login.setOnClickListener(view -> {
-            final String getmobile_num = mobile_no.getText().toString();
-            final String getenr_num = enr_no.getText().toString();
-            if(!getmobile_num.trim().isEmpty())
+            final String  Mobile_No = mobile_no.getText().toString();
+            final String Enrollment_No = enr_no.getText().toString();
+            if(Mobile_No.trim().length() == 10 && Enrollment_No.trim().length() == 12)
             {
-                if(getmobile_num.trim().length() != 10)
-                {
-                    Toast.makeText(student_login.this,"Invalid mobile number",Toast.LENGTH_LONG).show();
-                }
-                else{
-                    Intent i = new Intent(student_login.this,otp_verification.class);
-                    i.putExtra("mobile",getmobile_num);
-                    i.putExtra("class_name", class_name);
-                    startActivity(i);
-                    finish();
-                }
+//                presentInDataBase[0] = 0;
+//                ToastText[0] = "0";
+                String URL = "http://192.168.29.237/mysql/CheckforStudent.php";
+                RequestQueue queue = Volley.newRequestQueue(student_login.this);
+                StringRequest stringRequest = new StringRequest(Request.Method.POST,URL ,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONObject Jobj = new JSONObject(response);
+
+                                    if(Jobj.getString("result").equalsIgnoreCase("1"))
+                                    {
+                                        otp_verpage(Mobile_No,class_name);
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(student_login.this,Jobj.getString("result"), Toast.LENGTH_LONG).show();
+                                    }
+                                } catch (JSONException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(student_login.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }){
+                    @Override
+                    protected Map<String,String> getParams(){
+                        Map<String,String> params = new HashMap<String, String>();
+                        params.put("enrollment",Enrollment_No);
+                        params.put("mobile_no",Mobile_No);
+                        return params;
+                    }
+                    @Override
+                    public Map<String, String> getHeaders() throws AuthFailureError {
+                        Map<String,String> params = new HashMap<String, String>();
+                        params.put("Content-Type","application/x-www-form-urlencoded");
+                        return params;
+                    }
+                };
+                queue.add(stringRequest);
             }
             else{
-                Toast.makeText(student_login.this,"Enter mobile number",Toast.LENGTH_LONG).show();
+                if(Enrollment_No.trim().length() != 12 && Mobile_No.trim().length() != 10)
+                {
+                    Toast.makeText(student_login.this,"Incorrect Mobile No and Enrollment No",Toast.LENGTH_LONG).show();
+                }
+                else if(Mobile_No.trim().length() != 10)
+                {
+                    Toast.makeText(student_login.this,"Incorrect Mobile No",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Toast.makeText(student_login.this,"Incorrect Enrollment No",Toast.LENGTH_LONG).show();
+                }
             }
         });
+    }
+
+    private void otp_verpage(String Mobile_No,String class_name) {
+        Intent i = new Intent(student_login.this, otp_verification.class);
+        i.putExtra("mobile", Mobile_No);
+        i.putExtra("class_name", class_name);
+        startActivity(i);
+        finish();
     }
 }
