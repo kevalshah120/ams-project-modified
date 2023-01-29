ff0df087622dc09fff3ed09db65dd67289f075fd
Keval shah <kevalshah120@gmail.com>
2023-01-29 15:49:06 +0530

    attendance code alert_dialog done

--- app/src/main/java/com/example/splashscreen/stu_home_fragement.java
+++ app/src/main/java/com/example/splashscreen/stu_home_fragement.java
@@ -1,16 +1,15 @@
 package com.example.splashscreen;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +21,6 @@ import java.util.List;
  */
 public class stu_home_fragement extends Fragment {
 
-    int lecture_data_size;
     List<stu_lecture_model> lecture_data;
     private RecyclerView recyclerView;
 
--- app/src/main/java/com/example/splashscreen/stu_lecture_adapter.java
+++ app/src/main/java/com/example/splashscreen/stu_lecture_adapter.java
@@ -1,37 +1,63 @@
 package com.example.splashscreen;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.List;
 
 public class stu_lecture_adapter extends RecyclerView.Adapter<stu_lecture_adapter.MyViewHolder> {
     private List<stu_lecture_model> lecture_data;
+    Dialog dialog;
+    private String attend_code;
     Context context;
-    public stu_lecture_adapter(Context context,List<stu_lecture_model>lecture_data)
-    {
+
+    public stu_lecture_adapter(Context context, List<stu_lecture_model> lecture_data) {
         this.context = context;
         this.lecture_data = lecture_data;
     }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
-        View view = layoutInflater.inflate(R.layout.stu_lecture_cv,parent,false);
-        return new MyViewHolder(view);
+        View view = layoutInflater.inflate(R.layout.stu_lecture_cv, parent, false);
+        MyViewHolder viewHolder = new MyViewHolder(view);
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
         String subject_name = lecture_data.get(position).getSubject_name();
         String prof_name = lecture_data.get(position).getProf_name();
-        holder.setData(subject_name,prof_name);
+        holder.setData(subject_name, prof_name);
+        holder.markButton.setOnClickListener(view -> {
+            LayoutInflater inflater = LayoutInflater.from(context);
+            View dialogView = inflater.inflate(R.layout.attendance_code_dialog, null);
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setView(dialogView);
+            builder.setTitle("Attendance Code");
+            Button submit = dialogView.findViewById(R.id.submitbutton);
+            EditText attendance_code = dialogView.findViewById(R.id.attendance_code);
+            dialog = builder.create();
+            dialog.show();
+            submit.setOnClickListener(view1 -> {
+                attend_code = attendance_code.getText().toString();
+                Toast.makeText(context, attend_code, Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            });
+
+        });
     }
 
     @Override
@@ -42,15 +68,20 @@ public class stu_lecture_adapter extends RecyclerView.Adapter<stu_lecture_adapte
     public class MyViewHolder extends RecyclerView.ViewHolder {
         private final TextView textView1_subject_name;
         private final TextView textView2_prof_name;
+        private final Button markButton;
+
+
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             textView1_subject_name = itemView.findViewById(R.id.subject_name);
             textView2_prof_name = itemView.findViewById(R.id.prof_name);
+            markButton = itemView.findViewById(R.id.mark_button);
         }
 
-        public void setData(String subject_name,String prof_name) {
+        public void setData(String subject_name, String prof_name) {
             textView1_subject_name.setText(subject_name);
             textView2_prof_name.setText(prof_name);
         }
     }
+
 }
